83658d8e582068c7f9fb07746be16ce94e5265d5
nathan.osantos <nathan.osantos@CAS511908W1024.senacsp.edu.br>
2019-02-28 21:50:38 -0300

    Alteração da loja

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
@@ -3,5 +3,7 @@ package br.senac.tads.pi3.gerenprod;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Teste maven");
+        int i =0;
+        System.out.println("Valor i++= "+ i++);
     }
 }
